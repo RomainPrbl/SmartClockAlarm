87727842dc5961d2ab5db5b7bb669e026aea1caa
Romain <94350004+RomainPrbl@users.noreply.github.com>
2022-10-30 22:38:18 +0100

    structure principal et basique de l'affichage terminé

--- Tkinter.py
+++ Tkinter.py
@@ -1,31 +1,40 @@
 from tkinter import *
 from turtle import left
+from PIL import ImageTk, Image
 #tout est commenté => Romain meilleur dev 
 
 fenetre = Tk()
 fenetre.iconbitmap("style/reveil.ico")
 fenetre.title(string='SmartClock3000') # titre de la page
 
-#création des sous frame principales : (voir maquette.png)
-
 #containerRight 
 frameRIGHT = Frame(fenetre, borderwidth=2, relief=GROOVE)
 frameRIGHT.pack(side=RIGHT, padx=5, pady=5,expand=True,fill=BOTH)
-Label(frameRIGHT, text="Frame right").pack(padx=10, pady=10)
 
+#Creation des sous containers Right:
+frameStatus = Frame(frameRIGHT, borderwidth=2, relief=GROOVE)
+frameStatus.pack(side=TOP, padx=5, pady=5,expand=True,fill=BOTH)
+Label(frameStatus, text="Status de la connection :").pack(padx=10, pady=10)
+Label(frameStatus, text="connecte").pack()
 
+frameAlarmeDate = Frame(frameRIGHT, borderwidth=2, relief=GROOVE)
+frameAlarmeDate.pack(side=BOTTOM, padx=5, pady=5,expand=True,fill=BOTH)
+Label(frameAlarmeDate, text="La prochaine alarme et prevu pour : ").pack(padx=10, pady=10)
+Label(frameAlarmeDate, text="Vendredi 29 janvier à 8h30").pack()
 #containerLeft
 frameLeft = Frame(fenetre, borderwidth=2, relief=GROOVE)
 frameLeft.pack(side=LEFT, padx=5, pady=5,expand=True,fill=BOTH)
-Label(frameLeft, text="Frame Left").pack(padx=10, pady=10)
 
-#Les sous container de Left
+#sous container de Left : 
 frameEfreiLogo = Frame(frameLeft, borderwidth=2, relief=GROOVE)
 frameEfreiLogo.pack(side=TOP, padx=5, pady=5,expand=True,fill=BOTH)
-Label(frameEfreiLogo, text="Frame Logo Efrei").pack(padx=10, pady=10)
+
+img = ImageTk.PhotoImage(Image.open("style/efrei_logo.png").resize((200,100)))
+label = Label(frameEfreiLogo, image = img)
+label.pack()
 
 frameTRAA = Frame(frameLeft, borderwidth=2, relief=GROOVE) # TRAA = Temps restant avant alarme
 frameTRAA.pack(side=BOTTOM, padx=5, pady=5,expand=True,fill=BOTH)
-Label(frameTRAA, text="Frame TRAA").pack(padx=10, pady=10)
-
+Label(frameTRAA, text="Temps restant avant la prochaine alarme :").pack(padx=10, pady=10)
+Label(frameTRAA, text="2h 36min et 32s").pack()
 fenetre.mainloop()
