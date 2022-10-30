27e5884daaa8a23f5dcb8919ee5a6dfcb95d73af
Romain <94350004+RomainPrbl@users.noreply.github.com>
2022-10-30 13:18:10 +0100

    update de la sturucture des niveau
    le placement des frame sur tkinter me casse allègrement les couilles

--- Tkinter.py
+++ Tkinter.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from turtle import left
 #tout est commenté => Romain meilleur dev 
 
 fenetre = Tk()
@@ -9,22 +10,22 @@ fenetre.title(string='SmartClock3000') # titre de la page
 #création des trois sous frame principales : (voir maquette.png)
 
 #niveau 1
-frameNiv1 = Frame(fenetre,height=150, borderwidth=2,bg='red')
+frameNiv1 = Frame(fenetre,height=250, borderwidth=2,bg='red')
 frameNiv1.pack(fill=X)
 
 frameEfreiClock = Frame(frameNiv1,width=150,height=130, borderwidth=2,bg='purple')
-frameEfreiClock.pack()
+frameEfreiClock.pack(side=RIGHT)
 
 frameTempsAvtAlarme = Frame(frameNiv1,width=200,height=130, borderwidth=2,bg='orange')
-frameTempsAvtAlarme.pack()
+frameTempsAvtAlarme.pack(side=LEFT)
 
 #niveau 2
-frameNiv2 = Frame(fenetre,width=400,height=150,borderwidth=2,bg="blue")
-frameNiv2.pack()
+frameNiv2 = Frame(fenetre,height=150,borderwidth=2,bg="blue")
+frameNiv2.pack(fill=X)
 
 #niveau 3
-frameNiv3 = Frame(fenetre,width=400,height=300,borderwidth=2,bg="green")
-frameNiv3.pack()
+frameNiv3 = Frame(fenetre,height=300,borderwidth=2,bg="green")
+frameNiv3.pack(fill=X)
 
 
 
