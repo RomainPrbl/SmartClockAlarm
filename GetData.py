cd013ead40499fc6e5c118b143803742fe1c3c21
Romain <94350004+RomainPrbl@users.noreply.github.com>
2022-11-22 23:01:24 +0100

    ++ ecriture des focntions du txt

--- GetData.py
+++ GetData.py
@@ -6,7 +6,6 @@ import datetime
 from time import sleep,time
 
 
-
 def obtenirLienPlanning():
     data = {}
     data["agenda_url"] = input("Collez le lien ici : ").strip().replace("webcal://", "https://")
@@ -14,7 +13,6 @@ def obtenirLienPlanning():
     if r.status_code != 200:
         print("Erreur : Le lien que vous avez entré n'est pas valide.")
         obtenirLienPlanning()
-    print(" ")
     return data
 
 def passerDuLienAuCalendrier(data):
@@ -26,6 +24,7 @@ def recupererLesEvents(calendar):
 
     current_utc = pytz.UTC.localize(datetime.datetime.utcnow())
     events = []
+    eventaVenir = []
     for event in calendar.walk():
         if event.name == "VEVENT":
             start = event.get("dtstart").dt
@@ -37,10 +36,30 @@ def recupererLesEvents(calendar):
                 {"start": start, "end": end, "nom_cours": summary, "salle": location, "nom_prof": description})
     for event in events:
         if event["start"] > datetime.datetime.now(datetime.timezone.utc): # le datetime n'est pas naif !!!
-            print(event["start"],event["nom_cours"],type(event["start"]))
-            sleep(2)
-    return None
+            eventaVenir.append(event)
+    return eventaVenir
 
+def estDejaRempli():
+    # 0 fichier vide 1 sinon 
+    f = open("events.txt","r")
+    lines = f.readlines()
+    f.close
+    if len(lines) == 0 : 
+        return 0
+    else :
+        return 1
+
+def writeEventsInTxt(events):
+    f = open("events.txt","w") 
+    for dico in events :
+        f.write(str(dico) + '\n')
+    f.close()
+
+def recupEventsAvecTxt():
+    f = open("events.txt", "r")
+    lines = f.readlines()
+    f.close()
+    for i in line 
 if __name__ == "__main__":
-    print(recupererLesEvents(passerDuLienAuCalendrier(obtenirLienPlanning())))
+    writeEventsInTxt(recupererLesEvents(passerDuLienAuCalendrier(obtenirLienPlanning())))
 
---
+++ laboDeTest.py
@@ -0,0 +1,4 @@
+string = {"tel" : "0628562387"}
+string = str(string)
+print(string)
+string = dict(string)
