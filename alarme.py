19a42abf3a068ebf1829d6c5b1f2a05a0d39e112
MarleneSchiappaa <max.tonnadre@efrei.net>
2022-11-28 12:35:12 +0100

    alarmeIfTime

--- alarme.py
+++ alarme.py
@@ -1,6 +1,15 @@
 from playsound import playsound
+from gestionTemps import verifDate
 
 def alarme(son) :
     playsound(f'D:/projetAlarme/SmartClockAlarm/assets/{son}.mp3')
 
 alarme("sonMilitaire")
+
+
+def alarmeIfTime(dateToGet) :
+       if(verifDate(dateToGet)) :
+        alarme("sonReveilStandar")
+
+       
+    
